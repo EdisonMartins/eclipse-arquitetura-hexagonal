28ce46116078fff0ff8e83f2c622f4636d96faa1
Edison Martins <martinsegmj@gmail.com>
2022-04-06 03:03:51 -0300

    Primeiro commit

---
+++ arquitetura-hexagonal/src/main/java/dev/edisonmartins/arquitetura_hexagonal/App.java
@@ -0,0 +1,13 @@
+package dev.edisonmartins.arquitetura_hexagonal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
